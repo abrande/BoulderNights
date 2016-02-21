eb248bbcae4d3acc8f354b56634e9c7d1bc014cd
Ana-Sophia Brande <Sophie@rgnt2-84-23-dhcp.int.colorado.edu>
2016-02-20 18:23:04 -0700

    Initial Commit

---
+++ BoulderNights/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoulderNights
+//
+//  Created by Ana-Sophia Brande on 2/20/16.
+//  Copyright © 2016 T9Hacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BoulderNightsTests/BoulderNightsTests.swift
@@ -0,0 +1,36 @@
+//
+//  BoulderNightsTests.swift
+//  BoulderNightsTests
+//
+//  Created by Ana-Sophia Brande on 2/20/16.
+//  Copyright © 2016 T9Hacks. All rights reserved.
+//
+
+import XCTest
+@testable import BoulderNights
+
+class BoulderNightsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
