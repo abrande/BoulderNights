6b1ba11c21bf2fd97307cd1530385fe17157b888
Amanda <AG@rgnt2-121-177-dhcp.int.colorado.edu>
2016-02-21 04:28:29 -0700

    Profile along with front page and date selection

---
+++ BoulderNights/ViewThree.swift
@@ -0,0 +1,19 @@
+//
+//  ViewThree.swift
+//  BoulderNights
+//
+//  Created by Amanda on 2/21/16.
+//  Copyright © 2016 T9Hacks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewThree : UIViewController{
+    
+    @IBOutlet weak var ViewThree: UILabel!
+    
+    override func viewDidLoad(){
+        
+    }
+}
---
+++ BoulderNights/ViewTwo.swift
@@ -0,0 +1,19 @@
+//
+//  ViewTwo.swift
+//  BoulderNights
+//
+//  Created by Amanda on 2/21/16.
+//  Copyright © 2016 T9Hacks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewTwo : UIViewController{
+    
+    @IBOutlet weak var Date: UIDatePicker!
+
+    
+    override func viewDidLoad(){
+    }
+}
